0ee0099606089991f67852e7b694522ec1ff4edd
Ryan Manchester <ryanm17@gmail.com>
2021-04-01 13:11:04 -0400

    setting up next.js server

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+const Index = () => {
+  return(
+    <div>
+    <h1>Sample Shopify app using React and Nodejs</h1>
+    </div>
+  )
+
+}
+
+export default Index
---
+++ server.js
@@ -0,0 +1,41 @@
+require('isomorphic-fetch');
+const dotenv = require('dotenv');
+const Koa = require('koa');
+const next = require('next');
+const { default: createShopifyAuth } = require('@shopify/koa-shopify-auth');
+const { verifyRequest } = require('@shopify/koa-shopify-auth');
+const { default: Shopify, ApiVersion } = require('@shopify/shopify-api');
+const Router = require('koa-router');
+
+dotenv.config();
+
+Shopify.Context.initialize({
+  API_KEY: process.env.SHOPIFY_API_KEY,
+  API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
+  SCOPES: process.env.SHOPIFY_API_SCOPES.split( ),
+  HOST_NAME: process.env.SHOPIFY_APP_URL.replace(/https:\/\//, ""),
+  API_VERSION: ApiVersion.October20,
+  IS_EMBEDDED_APP: true;
+  SESSION_STORAGE: new Shopify.Session.MemorySessionStorage();
+});
+
+const port = parseInt(process.env.PORT, 10) || 30000;
+const dev = process.env.NODE_ENV !== 'production';
+const app = next({ dev });
+const handle = app.getRequestHandler();
+
+app.prepare().then(() => {})
+
+const server = new Koa();
+const router = new Router();
+server.keys = [Shopify.Context.API_SECRET_KEY];
+
+const handleRequest = async (ctx) => {
+  await handle(ctx.req, ctx.res);
+  ctx.respond = false;
+  ctx.res.statusCode = 200;
+};
+
+router.get('(.*)', handleRequest);
+server.use(router.allowedMethods());
+server.use(router.routes());
